d079a04e3f745b3aae91f45d8277f8f1ad7f6fce
wen.jie <wenjie204811@gmail.com>
2024-02-08 10:07:07 +0800

    字符串和内存函数实现

---
+++ source/kernel/include/tools/klib.h
@@ -0,0 +1,20 @@
+#ifndef __KLIB_H__
+#define __KLIB_H__
+
+#include "comm/types.h"
+
+void kernel_strcpy(char * dest, const char * src);
+
+void kernel_strncpy(char * dest, const char * src, int size);
+
+int kernel_strcmp(const char * s1, const char * s2, int size);
+
+int kernel_strlen(const char * str);
+
+void kernel_memcpy(void * dest, void * src, int size);
+
+void kernel_memset(void * dest, uint8_t * src, int size);
+
+int kernel_memcmp(void * d1, void * d2, int size);
+
+#endif
