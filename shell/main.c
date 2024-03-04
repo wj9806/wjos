7ff87115abe0fbeb52c7efcfa59b8eb21aa11b79
wen.jie <wenjie204811@gmail.com>
2024-03-04 09:32:56 +0800

    支持echo命令

--- shell/main.c
+++ shell/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <getopt.h>
 #include "main.h"
 #include "lib_syscall.h"
 
@@ -29,6 +30,62 @@ static int do_clear(int argc, char ** argv)
     return 0;
 }
 
+static int do_echo(int argc, char ** argv)
+{
+    if (argc == 1)
+    {
+        char msg_buf[128];
+        fgets(msg_buf, sizeof(msg_buf), stdin);
+        msg_buf[sizeof(msg_buf) - 1] = '\0';
+        puts(msg_buf);
+        return 0;
+    }
+    
+    int count = 1;
+    int ch;
+    while ((ch = getopt(argc, argv, "n:h")) != -1)
+    {
+        switch (ch)
+        {
+            //echo -h
+            case 'h':
+                puts("echo any message");
+                puts("Usage: echo [-n count] message");
+                // getopt需要多次调用，需要重置
+                optind = 1;
+                return 0;
+            case 'n':
+                //atoi 把字符串转换成整型数。
+                count = atoi(optarg);
+                break;
+            case '?':
+                if (optarg)
+                {
+                    fprintf(stderr, "Unknown option: -%s", optarg);
+                }
+                optind = 1;
+                return -1;
+            default:
+                break;
+        }
+    }
+    // 索引已经超过了最后一个参数的位置，意味着没有传入要发送的信息
+    if (optind > argc -1)
+    {
+        fprintf(stderr, "Message is empty \n");
+        optind = 1;
+        return -1;
+    }
+        // 循环打印消息
+    char * msg = argv[optind];
+    for (int i = 0; i < count; i++) {
+        
+        puts(msg);
+    }
+    optind = 1;
+    return 0;
+}
+
 static const cli_cmd_t cmd_list[] = {
     {
         .name = "help",
@@ -39,6 +96,11 @@ static const cli_cmd_t cmd_list[] = {
         .name = "clear",
         .usage = "clear: clear screen",
         .do_func = do_clear
+    },
+    {
+        .name = "echo",
+        .usage = "echo [-n count] msg",
+        .do_func = do_echo
     }
 };
 
